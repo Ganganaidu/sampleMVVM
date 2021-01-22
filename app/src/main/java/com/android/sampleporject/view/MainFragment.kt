8f786afb6aeae94d7776aeb1db9a01951890e4be
ramanach <ramana.chanduri@huskyenergy.com>
2021-01-21 20:14:00 -0600

    Added search functionality

--- app/src/main/java/com/android/sampleporject/view/MainFragment.kt
+++ app/src/main/java/com/android/sampleporject/view/MainFragment.kt
@@ -1,15 +1,17 @@
 package com.android.sampleporject.view
 
+import android.app.SearchManager
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.SearchView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.android.sampleporject.R
 import com.android.sampleporject.base.view.BaseFragment
 import com.android.sampleporject.viewmodel.MainViewModel
+
 
 class MainFragment : BaseFragment() {
 
@@ -28,12 +30,47 @@ class MainFragment : BaseFragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        setHasOptionsMenu(true)
         viewModel = ViewModelProvider(this, viewModelFactory).get(MainViewModel::class.java)
-        viewModel.getSearchData("car")
-
+        viewModel.doSearch()
         viewModel.searchLiveData.observe(viewLifecycleOwner, Observer {
             Log.d("data", "Test links ${it?.link}")
         })
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_search, menu)
+        val mSearchMenuItem = menu.findItem(R.id.search)
+        val searchManager =
+            activity?.getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = mSearchMenuItem.actionView as SearchView
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(activity?.componentName))
+        fromView(searchView)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.search ->
+                return false
+            else -> {
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun fromView(searchView: SearchView) {
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                viewModel.subject.onComplete()
+                searchView.clearFocus()
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                newText?.let { viewModel.subject.onNext(it) }
+                return true
+            }
+        })
+    }
 }
--- app/src/main/java/com/android/sampleporject/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/android/sampleporject/viewmodel/MainViewModel.kt
@@ -4,16 +4,34 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.android.sampleporject.apiservice.ApiService
 import com.android.sampleporject.model.SearchResponse
+import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
+import io.reactivex.subjects.PublishSubject
+import java.util.*
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
+
 
 class MainViewModel @Inject constructor(private var apiService: ApiService) : ViewModel() {
     val searchLiveData = MutableLiveData<SearchResponse>()
     private val disposable = CompositeDisposable()
+    val subject = PublishSubject.create<String>()
 
-    fun getSearchData(searchTag: String) {
+    fun doSearch() {
+        disposable.add(subject
+            .debounce(300, TimeUnit.MILLISECONDS)
+            .filter { text -> text.isNotBlank() && text.length >= 3 }
+            .map { text -> text.toLowerCase(Locale.getDefault()).trim() }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                getSearchData(it)
+            })
+    }
+
+    private fun getSearchData(searchTag: String) {
         disposable.add(
             apiService.searchData(searchTag)
                 .subscribeOn(Schedulers.io())
@@ -23,6 +41,7 @@ class MainViewModel @Inject constructor(private var apiService: ApiService) : Vi
                 }, { searchLiveData.value = null })
         )
     }
+
 
     override fun onCleared() {
         super.onCleared()
